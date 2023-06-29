6f73cffa4c3ed8f979840c0922da502f979d2738
Kyle Hodo <kyle.hodo@outlook.com>
2023-06-28 22:18:46 -0500

    created header for name of account

--- src/StockPortfolioApp.java
+++ src/StockPortfolioApp.java
@@ -1,19 +1,12 @@
-/*
-** Class Name: IFT-210
+import java.util.Scanner;
 
-** Author: Kyle hODO
-
-** Date Created: 06/12/2023
-
-** Purpose: This Application is to demonstrate
-         stock portfolio that allows you to do
-         transactions like buying and selling 
-         stock. With the ability to deposit and 
-         withdraw liek a brokerage account
-*/
 
 public class StockPortfolioApp {
     public static void main(String[] args) throws Exception {
+        Scanner scnr = new Scanner(new System.in);
+        String userName = scnr.next();
+        System.out.print(" Brokerage Account");
+        
         
     }
 }
