ad10255263dbebf7f0bf673a42e264a6cce18c6a
Kyle Hodo <kyle.hodo@outlook.com>
2023-06-28 22:17:24 -0500

    Created two classes for trans history and menu

---
+++ src/PortfolioManager.java
@@ -0,0 +1,19 @@
+/*
+** Class Name: IFT-210
+
+** Author: Kyle Hodo
+
+** Date Created: 06/12/2023
+
+** Purpose: This Application is to demonstrate
+         stock portfolio that allows you to do
+         transactions like buying and selling 
+         stock. With the ability to deposit and 
+         withdraw liek a brokerage account
+*/
+
+public class PortfolioManager {
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
---
+++ src/TransactionHistory.java
@@ -0,0 +1,63 @@
+
+public class TransactionHistory {
+    private String ticker, transDate, transType;
+    private double qty, costBasis;
+
+    public TransactionHistory() {
+        ticker = "";
+        transDate = "";
+        transType = "";
+        qty = 0.0;
+        costBasis = 1.00;
+
+
+    }
+
+    // getter methods 
+
+    public void getTicker() {
+
+    }
+
+    public void getTransDate() {
+
+    }
+
+    public void getTransType() {
+
+    }
+
+    public void getQty() {
+
+    }
+
+    public void getCostBasis() {
+
+    }
+
+    // setter methods
+
+    public String setTicker() {
+
+    }
+
+    public String setTransDate() {
+
+    }
+
+    public String setTransType() {
+
+    }
+
+    public double setQty() {
+
+    }
+
+    public double setCostBasis() {
+
+    }
+
+
+
+
+}
