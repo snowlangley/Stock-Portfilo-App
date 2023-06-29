70060ea9232fc8034b388e74ebb4ebe3c15227c4
Kyle Hodo <kyle.hodo@outlook.com>
2023-06-28 22:22:05 -0500

    redunant

--- src/PortfolioManager.java
+++ src/PortfolioManager.java
@@ -12,8 +12,12 @@
          withdraw liek a brokerage account
 */
 
+import java.util.Scanner;
+
 public class PortfolioManager {
     public static void main(String[] args) throws Exception {
-        
+        Scanner scnr = new Scanner(new System.in);
+        String userName = scnr.next();
+        System.out.print(" Brokerage Account");
     }
 }
